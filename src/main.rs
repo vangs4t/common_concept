41a3a5d718cdf72d0fef648c8125cdc6b9b59801
Jhoni Buraong <bj856575@gmail.com>
2024-12-03 05:19:48 +0700

    penambahan beberapa type integer literal

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,24 @@ fn tipe_data_integer(){
     let signed_1 : i32 = -14; // i32 signed
     let signed_2 : i32 = 13;
 
+    // ini adalah integer decimal, namun decimal di rust integer literal
+    // dipisahkan dengan underscore
+    let decimal : u32 = 9_22;
+    println!("This is decimal {decimal}");
+
+    // bilangan hex yang sering kita temukan dalam penggunaaan warna 
+    let hexadecimal = 0x2f;
+    println!("this is hexadecimal {hexadecimal}");
+
+    // bilangan octal merepresentasikan dalam penggunaan memori
+    let octals = 0o22;
+    println!("this is octal {octals}");
+
+    // bilangan biner biasanya digunakan untuk merepresentasikan suatu data
+    // di dalam bidang komputer
+    let binnary = 0b1101_0000;
+    println!("this is binnary {binnary}");
+
     println!("Ini adalah tipe data integer signed");
     println!("signed {signed_1}, {signed_2}");
     println!("ini adalah tipe data integer unsigned");
