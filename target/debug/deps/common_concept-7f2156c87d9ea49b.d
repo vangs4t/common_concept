db124b8011d09ab814809944c50263f25f1687bb
LENOVO <bj856575@gmail.com>
2024-12-02 12:57:25 +0700

    Initial commit

---
+++ target/debug/deps/common_concept-7f2156c87d9ea49b.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\Dev\RustProgramming\common_concept\target\debug\deps\libcommon_concept-7f2156c87d9ea49b.rmeta: src/main.rs
+
+C:\Users\LENOVO\Dev\RustProgramming\common_concept\target\debug\deps\common_concept-7f2156c87d9ea49b.d: src/main.rs
+
+src/main.rs:
